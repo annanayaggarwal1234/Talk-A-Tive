df40398dd0e0ccb29251caee4d3d262085334b49
Annanay Aggarwal <aggarwalannanay@gmail.com>
2026-04-15 10:51:32 +0530

    update changes

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ const app = express();
 app.use(express.json()); // to accept json data
 
 
-// app.get("/", (req, res) => {
-//   res.send("API Running!");
-// });
+app.get("/", (req, res) => {
+  res.send("API Running!");
+});
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
@@ -24,20 +24,9 @@ app.use("/api/message", messageRoutes);
 
 
 // --------------------------deployment------------------------------
-
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+app.get("/", (req, res) => {
+  res.send("API Running!");
+});
 
 // --------------------------deployment------------------------------
 
